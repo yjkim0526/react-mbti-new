ae505c453a9e45c891b63b1b9b464e2e3fdddfe2
yjkim0526 <75759670+yjkim0526@users.noreply.github.com>
2025-04-04 15:47:09 +0900

    Update Footer.jsx
    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="bg-gray-500 text-gray-200 py-4 mt-auto">
       <div className="container mx-auto px-4 text-center">
-        <p>© 2024 MBTI 테스트. All rights reserved.</p>
+        <p> © 2024 MBTI 테스트. All rights reserved.</p>
       </div>
     </footer>
   );
